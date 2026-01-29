59a4a602127b70e5c833e3c67cbe0cf67df1e015
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-01-30 00:39:28 +0530

    multiplication

---
+++ mult.java
@@ -0,0 +1,11 @@
+public class mult {
+        public static void main(String[] args) {
+        float a = 10;
+        float b = 20;
+
+        float mult = a * b;
+        System.out.println(mult);
+    }
+
+    
+}
