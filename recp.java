d81278ab5b371b451dcaed9219a915bdc294c272
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-01-31 01:01:13 +0530

    perimeter of rectangle

---
+++ recp.java
@@ -0,0 +1,10 @@
+public class recp {
+    public static void main(String[] args) {
+       int l = 20;
+       int b = 10;
+       int area = 2*(l+b);
+        System.out.println(area);
+    }
+
+
+}
