a90c39efcfde6585b7fadb4a0df42d9b72474739
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-01 10:59:10 +0530

    integer input

---
+++ java.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();   
+        System.out.println(input);
+    }
+
+}
