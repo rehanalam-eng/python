e928f570b07aa8ce8a8656b98cdf4b809bc8d284
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-03 00:53:56 +0530

    perimeter of square

---
+++ perimeterofsquare.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class perimeterofsquare{
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int perimeter = 4*a;
+      System.out.println(perimeter);
+    }
+}
