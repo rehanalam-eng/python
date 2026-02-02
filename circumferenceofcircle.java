24b2426156443a6e9e6c2da12f522c31aef6179f
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-03 00:58:36 +0530

    circumference of circle

---
+++ circumferenceofcircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class circumferenceofcircle{
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      float r = sc.nextInt();
+      float circumference = 2*3.14f*r;
+      System.out.println(circumference);
+    }
+}
