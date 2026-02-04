1d2e79caa7a72f86015c89b4367d186502601a49
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-04 11:36:32 +0530

    average of three numbers

---
+++ avgofthree.java
@@ -0,0 +1,9 @@
+public class avgofthree{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        int average =(a+b+c)/3;
+        System.err.println(average);
+    }
+}
