60572ba88370a58942a9da2f503c444e98fee2da
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-12 00:23:55 +0530

    table of 5

---
+++ multof5.java
@@ -0,0 +1,9 @@
+public class multof5 {
+    public static void main(String[] args) {
+         System.out.println("Table of  5 is =");
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(5*i);
+        }
+       
+    }
+}
