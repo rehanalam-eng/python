6db980c152b9509ea116027b38104a0f3f75193a
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-12 00:25:15 +0530

    table of 5 using while loop

---
+++ tableusingwhileloop.java
@@ -0,0 +1,9 @@
+public class tableusingwhileloop {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 10) {
+            System.out.println("5 x " + i + " = " + (5*i));
+            i++;
+        }
+    }
+}
