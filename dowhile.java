2879ef5084adbd75d0a0271e39c307bc4a26edbc
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-12 23:13:31 +0530

    do while loop for print hello world

---
+++ dowhile.java
@@ -0,0 +1,9 @@
+public class dowhile{
+    public static void main(String[]a){
+        int counter =1;
+        do { 
+            System.out.println("Hello World");
+            counter ++;
+        } while (counter <=10);
+    }
+}
