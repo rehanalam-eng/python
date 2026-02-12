b2a4225f0a4a081a80a9150fb7bfc04ae2dd0677
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-13 00:13:13 +0530

    continue statement

---
+++ cont.java
@@ -0,0 +1,10 @@
+public class cont {
+    public static void main(String[] args) {
+        for (int i =0;i<10;i++){
+            if(i==3){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
