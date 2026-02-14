acefaf9c6ba9c4d67bdee9dd36a44cf5106d7343
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-14 23:59:32 +0530

    table of a number enter by user

--- practice.java
+++ practice.java
@@ -1,8 +1,7 @@
-public class practice{
-    public static void main(String[] args){ 
-        for(int i=0; i<5; i++) { 
-            System.out.println("Hello"); 
-            i+=2; 
-        } 
-    }
+public class practice { 
+public static void main(String args[]) { 
+for(int i = 0; i <= 5; i++ ) { 
+System.out.println("i = " + i ); 
+}System.out.println("i after the loop = " + i ); 
+} 
 }
