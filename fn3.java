2cbcaed4606f358a101a67600c9114f83d3fd8f5
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-18 17:17:42 +0530

    functions using parameters

---
+++ fn3.java
@@ -0,0 +1,11 @@
+public class fn3 {
+
+    static int square(int n) {
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        int result = square(4);
+        System.out.println("Square = " + result);
+    }
+}
