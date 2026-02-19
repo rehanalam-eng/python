46cd190cadacebaf9d17c4db20b3d157d15cca01
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-20 00:08:28 +0530

    functions using parameters ,area of circle

---
+++ fnAreaofCircle.java
@@ -0,0 +1,11 @@
+public class fnAreaofCircle {
+
+    static void AreaOfCircle(float r) {
+        float area = 3.14f * r * r;  
+        System.out.println("area of circle = " + area);
+    }
+
+    public static void main(String[] args) {
+        AreaOfCircle(4);
+    }
+}
