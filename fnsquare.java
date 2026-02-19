c44440d643d6ebfe43366428870adc0e56725139
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-02-20 00:02:06 +0530

    functions using parameters ,area of square

---
+++ fnsquare.java
@@ -0,0 +1,11 @@
+public class fnsquare{
+
+    static void AreaOfSquare(int a) {
+        int area = a*a;
+        System.out.println("Modulus= " + area);
+    }
+
+    public static void main(String[] args) {
+        AreaOfSquare(4);
+    }
+}
