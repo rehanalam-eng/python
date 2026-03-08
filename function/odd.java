ce8c98655e0387ccb6eab6b042c0290ed3f312ae
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-03-08 22:49:05 +0530

    odd even

---
+++ function/odd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class odd {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if(num % 2 == 0) {
+            System.out.println("Number is Even");
+        } 
+        else {
+            System.out.println("Number is Odd");
+        }
+
+        sc.close();
+    }
+}
