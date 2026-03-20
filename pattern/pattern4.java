48ea22717270a4334f82eec4a0bb523e3c8247c6
Rehan Alam <rehanalam.kmclu@gmail.com>
2026-03-21 00:05:51 +0530

    pattern printing

---
+++ pattern/pattern4.java
@@ -0,0 +1,20 @@
+
+public class pattern4 {
+
+    // Function to print pattern
+    public static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        printPattern(n); // function call
+    }
+}
